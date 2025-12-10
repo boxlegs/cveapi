06076c39cdfbe60e676e61bc2b1ea38c102dc9bd
boxlegs <tobyturns133@gmail.com>
2025-12-10 14:42:45 +1000

    Added  CVE parsing, CVSS models

--- cveapi.py
+++ cveapi.py
@@ -1,18 +1,38 @@
 import requests
 from models import CVE
         
-    
+
 def call_nvd_api(parameters):
     return requests.get('https://services.nvd.nist.gov/rest/json/cves/2.0', params=parameters)
 
 def get_cve_by_id(cve_id):
     req = call_nvd_api({'cveId': cve_id})
     if req.status_code == 200:
-        return CVE(req.json().get('vulnerabilities')[0].get('cve'))
+        return parse_cve(req.json())
     
 def get_cves_by_tag(cve_tag):
     req = call_nvd_api({'cveTag': cve_tag})
     if req.status_code == 200:
-        return req.json()
+        return parse_cves(req.json())
 
+def get_cves_by_severity(severity, version='4'):
 
+    sev_param = {f'cvssV{version}Severity': severity} 
+
+    req = call_nvd_api(sev_param)
+    if req.status_code == 200:
+        return parse_cves(req.json())
+
+def parse_cve(data: dict) -> CVE:
+    """
+    Parse a single CVE entry from NVD API response.
+    
+    :param data: Dictionary containing CVE data. 
+    :return: CVE object
+    """
+
+    return CVE(data.get('cve'))
+
+def parse_cves(data: dict) -> list[CVE]:
+    return [parse_cve(cve) for cve in data.get('vulnerabilities')]
+        
--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class CVE:
                     pass
                     parsed_metrics['2'] = CVSSv2Metric(metric)
                 case '4.0':
-                    # parsed_metrics['4'] = CVSSv4Metric(metric)
+                    parsed_metrics['4'] = CVSSv4Metric(metric)
                     pass
         return parsed_metrics
     
@@ -56,8 +56,6 @@ class CVSSMetric:
     version: str
     baseScore: str
     baseSeverity: str
-    exploitabilityScore: str
-    impactScore: str
 
     # Impact Metrics
     confidentialityImpact: str
@@ -69,14 +67,12 @@ class CVSSMetric:
         self.type = data['type']
         self.version = data['cvssData']['version']
         self.baseScore = data['cvssData']['baseScore']
-        self.exploitabilityScore = data['exploitabilityScore']
-        self.impactScore = data['impactScore']
-        self.confidentialityImpact = data['cvssData']['confidentialityImpact']
-        self.integrityImpact = data['cvssData']['integrityImpact']
-        self.availabilityImpact = data['cvssData']['availabilityImpact']
 
     def vectorString(self):
         pass
+
+    def _short(self, attr):
+        return 'X' if attr == 'NOT_DEFINED' else attr[0]
 
     def __str__(self):
         return f"{self.vectorString()} | Score {self.baseScore}"
@@ -91,6 +87,8 @@ class CVSSv2Metric(CVSSMetric):
     obtainUserPrivilege: bool
     obtainOtherPrivilege: bool
     userInteractionRequired: bool
+    exploitabilityScore: str
+    impactScore: str
 
     def __init__(self, data):
         super().__init__(data)
@@ -99,11 +97,17 @@ class CVSSv2Metric(CVSSMetric):
         self.accessComplexity = data['cvssData']['accessComplexity']
         self.authentication = data['cvssData']['authentication']
         self.acInsufInfo = data['acInsufInfo']
+        self.exploitabilityScore = data['exploitabilityScore']
         self.obtainAllPrivilege = data['obtainAllPrivilege']
         self.obtainUserPrivilege = data['obtainUserPrivilege']
         self.obtainOtherPrivilege = data['obtainOtherPrivilege']
-        self.userInteractionRequired = data['userInteractionRequired']
-    
+        # self.userInteractionRequired = data['userInteractionRequired']
+        self.impactScore = data['impactScore']
+        self.confidentialityImpact = data['cvssData']['confidentialityImpact']
+        self.integrityImpact = data['cvssData']['integrityImpact']
+        self.availabilityImpact = data['cvssData']['availabilityImpact']
+
+        
     def vectorString(self):
         return f"CVSS:{self.version}/AV:{self.accessVector[0]}/AC:{self.accessComplexity[0]}/Au:{self.authentication[0]}/C:{self.confidentialityImpact[0]}/I:{self.integrityImpact[0]}/A:{self.availabilityImpact[0]}"
 
@@ -118,6 +122,8 @@ class CVSSv3Metric(CVSSMetric): # Same class for v3.0 and v3.1
     privilegesRequired: str
     userInteraction: str
     scope: str
+    exploitabilityScore: str
+    impactScore: str
 
     def __init__(self, data):
         super().__init__(data)
@@ -127,8 +133,115 @@ class CVSSv3Metric(CVSSMetric): # Same class for v3.0 and v3.1
         self.attackComplexity = data['cvssData']['attackComplexity']
         self.privilegesRequired = data['cvssData']['privilegesRequired']    
         self.userInteraction = data['cvssData']['userInteraction']
+        self.exploitabilityScore = data['exploitabilityScore']
         self.scope = data['cvssData']['scope']
+        self.impactScore = data['impactScore']
+        self.confidentialityImpact = data['cvssData']['confidentialityImpact']
+        self.integrityImpact = data['cvssData']['integrityImpact']
+        self.availabilityImpact = data['cvssData']['availabilityImpact']
 
+        
     def vectorString(self):
         return f"CVSS:{self.version}/AV:{self.attackVector[0]}/AC:{self.attackComplexity[0]}/PR:{self.privilegesRequired[0]}/UI:{self.userInteraction[0]}/S:{self.scope[0]}/C:{self.confidentialityImpact[0]}/I:{self.integrityImpact[0]}/A:{self.availabilityImpact[0]}"
         
+class CVSSv4Metric(CVSSMetric):
+    
+    attackVector: str
+    attackComplexity: str
+    attackRequirements: str
+    privilegesRequired: str
+
+    def __init__(self, data):
+        super().__init__(data)
+
+        self.baseSeverity = data['cvssData']['baseSeverity']
+        self.attackVector = data['cvssData']['attackVector']
+        self.attackComplexity = data['cvssData']['attackComplexity']
+        self.attackRequirements = data['cvssData']['attackRequirements']
+        self.privilegesRequired = data['cvssData']['privilegesRequired']
+        self.userInteraction = data['cvssData']['userInteraction']
+        self.vulnConfidentialityImpact = data['cvssData']['vulnConfidentialityImpact']
+        self.vulnIntegrityImpact = data['cvssData']['vulnIntegrityImpact']
+        self.vulnAvailabilityImpact = data['cvssData']['vulnAvailabilityImpact']
+        self.subConfidentialityImpact = data['cvssData']['subConfidentialityImpact']
+        self.subIntegrityImpact = data['cvssData']['subIntegrityImpact']
+        self.subAvailabilityImpact = data['cvssData']['subAvailabilityImpact']
+        self.exploitMaturity = data['cvssData']['exploitMaturity']
+        self.confidentialityRequirement = data['cvssData']['confidentialityRequirement']
+        self.integrityRequirement = data['cvssData']['integrityRequirement']
+        self.availabilityRequirement = data['cvssData']['availabilityRequirement']
+        self.modifiedAttackVector = data['cvssData']['modifiedAttackVector']
+        self.modifiedAttackComplexity = data['cvssData']['modifiedAttackComplexity']
+        self.modifiedAttackRequirements = data['cvssData']['modifiedAttackRequirements']
+        self.modifiedPrivilegesRequired = data['cvssData']['modifiedPrivilegesRequired']
+        self.modifiedUserInteraction = data['cvssData']['modifiedUserInteraction']
+        self.modifiedVulnConfidentialityImpact = data['cvssData']['modifiedVulnConfidentialityImpact']
+        self.modifiedVulnIntegrityImpact = data['cvssData']['modifiedVulnIntegrityImpact']
+        self.modifiedVulnAvailabilityImpact = data['cvssData']['modifiedVulnAvailabilityImpact']
+        self.modifiedSubConfidentialityImpact = data['cvssData']['modifiedSubConfidentialityImpact']
+        self.modifiedSubIntegrityImpact = data['cvssData']['modifiedSubIntegrityImpact']
+        self.modifiedSubAvailabilityImpact = data['cvssData']['modifiedSubAvailabilityImpact']
+        self.safety = data['cvssData']['Safety']
+        self.automatable = data['cvssData']['Automatable']
+        self.recovery = data['cvssData']['Recovery']
+        self.valueDensity = data['cvssData']['valueDensity']
+        self.vulnerabilityResponseEffort = data['cvssData']['vulnerabilityResponseEffort']
+        self.providerUrgency = data['cvssData']['providerUrgency']
+
+        print(data['cvssData']['vectorString'])
+
+        print(self.vectorString())
+   
+    def vectorString(self):
+
+        baseMetrics = (
+                        f"/AV:{self._short(self.attackVector)}"
+                        f"/AC:{self._short(self.attackComplexity)}"
+                        f"/AT:{self._short(self.attackRequirements)}"
+                        f"/PR:{self._short(self.privilegesRequired)}"
+                        f"/UI:{self._short(self.userInteraction)}"
+                    )
+        
+        threatMetrics = (
+                        f"/VC:{self._short(self.vulnConfidentialityImpact)}"
+                        f"/VI:{self._short(self.vulnIntegrityImpact)}"
+                        f"/VA:{self._short(self.vulnAvailabilityImpact)}"
+        )
+        
+        subMetrics = (
+                        f"/SC:{self._short(self.subConfidentialityImpact)}"
+                        f"/SI:{self._short(self.subIntegrityImpact)}"
+                        f"/SA:{self._short(self.subAvailabilityImpact)}"
+        )
+
+        maturityMetrics = f"/E:{self._short(self.exploitMaturity)}"
+        environmentalMetrics = (
+                        f"/CR:{self._short(self.confidentialityRequirement)}"
+                        f"/IR:{self._short(self.integrityRequirement)}"
+                        f"/AR:{self._short(self.availabilityRequirement)}"
+        )
+
+        modifiedMetrics = (
+                        f"/MAV:{self._short(self.modifiedAttackVector)}"
+                        f"/MAC:{self._short(self.modifiedAttackComplexity)}"
+                        f"/MAR:{self._short(self.modifiedAttackRequirements)}"
+                        f"/MPR:{self._short(self.modifiedPrivilegesRequired)}"
+                        f"/MUI:{self._short(self.modifiedUserInteraction)}"
+                        f"/MVC:{self._short(self.modifiedVulnConfidentialityImpact)}"
+                        f"/MVI:{self._short(self.modifiedVulnIntegrityImpact)}"
+                        f"/MVA:{self._short(self.modifiedVulnAvailabilityImpact)}"
+                        f"/MSC:{self._short(self.modifiedSubConfidentialityImpact)}"
+                        f"/MSI:{self._short(self.modifiedSubIntegrityImpact)}"
+                        f"/MSA:{self._short(self.modifiedSubAvailabilityImpact)}"
+        )
+
+        suppMetrics = (
+                        f"/S:{self._short(self.safety)}"
+                        f"/AU:{self._short(self.automatable)}"
+                        f"/R:{self._short(self.recovery)}"
+                        f"/V:{self._short(self.valueDensity)}"
+                        f"/RE:{self._short(self.vulnerabilityResponseEffort)}"
+                        f"/U:{self._short(self.providerUrgency)}"
+        )
+
+        return f"CVSS:{self.version}" + baseMetrics + threatMetrics + subMetrics + maturityMetrics + environmentalMetrics + modifiedMetrics + suppMetrics
